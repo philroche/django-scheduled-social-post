fbe4f41e4cb8879782093c64ff0f810266043753
Philip Roche <phil.roche@ticket-text.com>
2015-09-07 16:10:19 +0100

    initial commit

--- README.rst
+++ README.rst
@@ -5,15 +5,8 @@ django-scheduled-social-post
 .. image:: https://badge.fury.io/py/django-scheduled-social-post.png
     :target: https://badge.fury.io/py/django-scheduled-social-post
 
-.. image:: https://travis-ci.org/philroche/django-scheduled-social-post.png?branch=master
-    :target: https://travis-ci.org/philroche/django-scheduled-social-post
-
 App to aid in scheduling social media posts: tweets, facebook messages etc.
 
-Documentation
--------------
-
-The full documentation is at https://django-scheduled-social-post.readthedocs.org.
 
 Quickstart
 ----------
--- scheduled_social_post/models.py
+++ scheduled_social_post/models.py
@@ -1,1 +1,47 @@
 # -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+
+class KeyValueManager(models.Manager):
+
+
+    def has_keyvalue(self, owner=None, key=None, co_owner=None):
+        """
+        Test if a given owner has an instance of
+        keyvalue for all the names provided
+        """
+        if owner and key:
+            qs = self.get_queryset()
+            owner_content_type_id = ContentType.objects.get_for_model(owner).id
+            owner_id = owner.id
+            filter_args = {'owner_content_type_id': owner_content_type_id,
+                           'owner_object_id': owner_id,
+                           'co_owner_content_type_id': None,
+                           'co_owner_object_id': None,
+                           'key': key }
+
+            if co_owner:
+                co_owner_content_type_id =ContentType.objects.get_for_model(co_owner).id
+                co_owner_id = co_owner.id
+                filter_args['co_owner_content_type_id'] = co_owner_content_type_id
+                filter_args['co_owner_object_id'] = co_owner_id
+
+            return qs.filter(**filter_args).exists()
+        else:
+            return False
+
+
+class ScheduledSocialPost(models.Model):
+
+    # Model instance that is the owner of this key value
+    owner_content_type_id = models.PositiveIntegerField(db_index=True)
+    owner_object_id = models.PositiveIntegerField(db_index=True)
+    service = twitter|facebook
+    service_token = char
+    service_user = char
+    content = text
+    sent = bool
+    schedule = datetime
+    created = datetime
